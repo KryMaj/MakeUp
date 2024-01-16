5830713ab5975e5f1546832b9d501c2112269a12
KryMaj <krystianmajda96@gmail.com>
2024-01-16 22:41:49 +0100

    pierwszy commit

---
+++ src/test/java/com/makeup/demo/MakeupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makeup.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
